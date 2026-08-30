37782a16b1e9beb4965d4f1e71c51c02095516fa
roshansky <roshansky1@gmail.com>
2026-08-30 20:06:30 +0300

    Update agent_report.py

--- agent_report.py
+++ agent_report.py
@@ -50,7 +50,7 @@ print("Отправка данных в Gemini...")
 
 # Новый синтаксис вызова модели
 response = client.models.generate_content(
-    model= 'gemini-2.0-flash',
+    model= 'gemini-3.0-flash',
     contents=system_prompt
 )
 
